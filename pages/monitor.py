37201e8af7978e50f7b6c2a87384eba2eb96160a
MShashank0511 <ms21chb0a28@student.nitw.ac.in>
2025-05-16 12:29:22 +0530

    Update monitor.py

--- pages/monitor.py
+++ pages/monitor.py
@@ -7,7 +7,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from back import process_models_from_session  # <-- Use this function
+from backend import process_models_from_session  # <-- Use this function
 
 
 st.title("Model Prediction Monitoring: Profitability, Charge-Off, Prepayment")
